7a0b5f8518c35a028ccbb25152e941800b602166
huybq886 <huybuiquang886@gmail.com>
2020-11-17 12:32:36 +0700

    Revert "Test github 3"
    This reverts commit 12f9356226dbc99f0c43f68d5919bb44c8ca6ef8.

--- CoursePlanRecommender/src/Main.java
+++ CoursePlanRecommender/src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		final int minGH = 96;
+		final int minGH = 96; 
 		AllCourses allCourses = new AllCourses(); 
 		allCourses.readFile("allCourses.txt");
 		Map<Course, List<Course>> electiveGraph = allCourses.toAdjacencyList();
