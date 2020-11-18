3adb6920d01d259c951d66a1b9335de3006ca507
Amy Nguyen <nguyen47@miamioh.edu>
2020-11-18 19:12:45 +0700

    Updata input dataset

--- CoursePlanRecommender/src/Main.java
+++ CoursePlanRecommender/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 //		printGraph(electiveGraph);
 
 		//String path = "/Users/buihq/Desktop/Huy Bui/IntelliJ/Course-Plan-Recommender/CoursePlanRecommender/allCourses.txt";
-		String path = "mandatoryCourses.txt";
+		String path = "majorCourses.txt";
 		CourseGraph majorGraph = new CourseGraph(path);
 
 		majorGraph.printGraph();
@@ -25,15 +25,27 @@ public class Main {
 
 	public static void getMiamiPlan() {
 		CourseSet foundationI = new CourseSet("foundationI.txt"); 
-		System.out.println(foundationI.getAllCourses());
+		System.out.println("Foundation I: " + foundationI.getAllCourses());
 		
 		CourseSet foundationIIA = new CourseSet("foundationIIA.txt"); 
-		System.out.println(foundationIIA.getAllCourses());
+		System.out.println("Foundation IIA: " + foundationIIA.getAllCourses());
 		
 		CourseSet foundationIIB = new CourseSet("foundationIIB.txt"); 
-		System.out.println(foundationIIB.getAllCourses());
+		System.out.println("Foundation IIB: " + foundationIIB.getAllCourses());
 		
 		CourseSet foundationIIC = new CourseSet("foundationIIC.txt"); 
-		System.out.println(foundationIIC.getAllCourses());
+		System.out.println("Foundation IIC: " + foundationIIC.getAllCourses());
+		
+		CourseSet foundationIII = new CourseSet("foundationIII.txt"); 
+		System.out.println("Foundation III: " + foundationIII.getAllCourses());
+		
+		CourseSet foundationIVA = new CourseSet("foundationIVA.txt"); 
+		System.out.println("Foundation IVA: " + foundationIVA.getAllCourses());
+		
+		CourseSet foundationIVB = new CourseSet("foundationIVB.txt"); 
+		System.out.println("Foundation IVB: " + foundationIVB.getAllCourses());
+		
+		CourseSet foundationV = new CourseSet("foundationV.txt"); 
+		System.out.println("Foundation V: " + foundationV.getAllCourses());
 	}
 }
