1d83ee3ebb8360395f928b58888cf9aeb3a20463
Amy Nguyen <nguyen47@miamioh.edu>
2020-11-18 18:16:48 +0700

    Miami Plan input dataset

--- CoursePlanRecommender/src/Main.java
+++ CoursePlanRecommender/src/Main.java
@@ -8,15 +8,32 @@ public class Main {
 //		Map<Course, List<Course>> electiveGraph = allCourses.toAdjacencyList();
 //		printGraph(electiveGraph);
 
-		String path = "/Users/buihq/Desktop/Huy Bui/IntelliJ/Course-Plan-Recommender/CoursePlanRecommender/allCourses.txt";
-		CourseGraph graph = new CourseGraph(path);
+		//String path = "/Users/buihq/Desktop/Huy Bui/IntelliJ/Course-Plan-Recommender/CoursePlanRecommender/allCourses.txt";
+		String path = "mandatoryCourses.txt";
+		CourseGraph majorGraph = new CourseGraph(path);
 
-		graph.printGraph();
+		majorGraph.printGraph();
 
-		graph.addElectives();
+		majorGraph.addElectives();
 
-		graph.printGraph();
+		majorGraph.printGraph();
+		
+		getMiamiPlan();
+		
 	}
 
 
+	public static void getMiamiPlan() {
+		CourseSet foundationI = new CourseSet("foundationI.txt"); 
+		System.out.println(foundationI.getAllCourses());
+		
+		CourseSet foundationIIA = new CourseSet("foundationIIA.txt"); 
+		System.out.println(foundationIIA.getAllCourses());
+		
+		CourseSet foundationIIB = new CourseSet("foundationIIB.txt"); 
+		System.out.println(foundationIIB.getAllCourses());
+		
+		CourseSet foundationIIC = new CourseSet("foundationIIC.txt"); 
+		System.out.println(foundationIIC.getAllCourses());
+	}
 }
